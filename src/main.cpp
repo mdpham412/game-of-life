b33bf35c039120e16c3af13a97edf08d5013ed53
Pham Minh Dung <224001528+mdpham412@users.noreply.github.com>
2025-09-01 18:17:26 +0700

    fixed cmakefiles

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,20 @@
-int main() {}
+#include "raylib.h"
+#include "rlImGui.h"
+
+int main() {
+  InitWindow(800, 450, "raylib [core] example - basic window");
+  rlImGuiSetup(true);
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    rlImGuiBegin();
+
+    rlImGuiEnd();
+    EndDrawing();
+  }
+
+  rlImGuiShutdown();
+  CloseWindow();
+
+  return 0;
+}
