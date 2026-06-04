c1ffdb471ea67a4f0d3be6401749b6f6a9aae480
Pham Minh Dung <224001528+mdpham412@users.noreply.github.com>
2026-06-04 19:47:02 +0700

    square activation and drawing

--- src/game.cpp
+++ src/game.cpp
@@ -7,6 +7,8 @@ Game::Game(int screenWidth, int screenHeight) {
   rlImGuiSetup(true);
   ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_DockingEnable;
   SetExitKey(KEY_NULL);
+
+  this->updateGridSize();
 }
 Game::~Game() {
   rlImGuiShutdown();
@@ -14,21 +16,9 @@ Game::~Game() {
 }
 void Game::loop() {
   while (!WindowShouldClose()) {
-    // frame start
-    BeginDrawing();
-    ClearBackground(backgroundColour);
-    rlImGuiBegin();
-
-    // frame drawing
-    this->drawGrid();
-    this->options();
-
-    DrawFPS(0, 0);
-
-    // frame end
-    ImGui::End();
-    rlImGuiEnd();
-    EndDrawing();
+    this->updateColors();
+    this->checkForSquareActivation();
+    this->draw();
   }
 }
 void Game::options() {
@@ -36,30 +26,93 @@ void Game::options() {
     ImGui::Text("Grid Customization");
     bool rows_changed = ImGui::SliderInt("Rows", &rows, 10, 200);
     bool cols_changed = ImGui::SliderInt("Columns", &cols, 10, 200);
+    if (rows_changed || cols_changed) {
+      this->updateGridSize();
+    }
+    ImGui::ColorEdit4("Background Color", (float*)&this->colourBackground);
+    ImGui::ColorEdit4("Lines Color", (float*)&this->colourLines);
+    ImGui::ColorEdit4("Squares Color", (float*)&this->colourSquares);
     ImGui::Separator();
     ImGui::Text("Simulation Controls");
   }
 }
+void Game::draw() {
+  // frame start
+  BeginDrawing();
+  ClearBackground(backgroundColour);
+  rlImGuiBegin();
+  ImGui::DockSpaceOverViewport(0, NULL, ImGuiDockNodeFlags_PassthruCentralNode);
+
+  // frame drawing
+  this->drawGrid();
+  this->options();
+
+  DrawFPS(0, 0);
+
+  // frame end
+  ImGui::End();
+  rlImGuiEnd();
+  EndDrawing();
+}
+void Game::updateColors() {
+  backgroundColour.r = (unsigned char)(this->colourBackground[0] * 255);
+  backgroundColour.g = (unsigned char)(this->colourBackground[1] * 255);
+  backgroundColour.b = (unsigned char)(this->colourBackground[2] * 255);
+  backgroundColour.a = (unsigned char)(this->colourBackground[3] * 255);
+  lineColour.r = (unsigned char)(this->colourLines[0] * 255);
+  lineColour.g = (unsigned char)(this->colourLines[1] * 255);
+  lineColour.b = (unsigned char)(this->colourLines[2] * 255);
+  lineColour.a = (unsigned char)(this->colourLines[3] * 255);
+  squareColour.r = (unsigned char)(this->colourSquares[0] * 255);
+  squareColour.g = (unsigned char)(this->colourSquares[1] * 255);
+  squareColour.b = (unsigned char)(this->colourSquares[2] * 255);
+  squareColour.a = (unsigned char)(this->colourSquares[3] * 255);
+}
+void Game::updateGridSize() {
+  squareSize = std::min(GetScreenWidth() / cols, GetScreenHeight() / rows) - 1;
+
+  gridWidth = cols * squareSize;
+  gridHeight = rows * squareSize;
+
+  paddingX = (GetScreenWidth() - gridWidth) / 2;
+  paddingY = (GetScreenHeight() - gridHeight) / 2;
+  activated.assign(rows * cols, false);
+}
+int Game::getGridIndex(int row, int col) { return row * cols + col; }
 void Game::drawGrid() {
-  int squareSize =
-      std::min(GetScreenWidth() / cols, GetScreenHeight() / rows) - 1;
-
-  int gridWidth = cols * squareSize;
-  int gridHeight = rows * squareSize;
-
-  // space between grid and window border
-  int paddingX = (GetScreenWidth() - gridWidth) / 2;
-  int paddingY = (GetScreenHeight() - gridHeight) / 2;
-
+  // draw activaated squares
+  for (int row = 0; row < rows; row++) {
+    for (int col = 0; col < cols; col++) {
+      if (activated[getGridIndex(row, col)]) {
+        DrawRectangle(paddingX + col * squareSize, paddingY + row * squareSize,
+                      squareSize, squareSize, squareColour);
+      }
+    }
+  }
   // horizontal lines
   for (int i = 0; i <= rows; i++) {
     DrawLine(paddingX, paddingY + i * squareSize, paddingX + gridWidth,
-             paddingY + i * squareSize, lineColor);
+             paddingY + i * squareSize, lineColour);
   }
 
   // vertical lines
   for (int i = 0; i <= cols; i++) {
     DrawLine(paddingX + i * squareSize, paddingY, paddingX + i * squareSize,
-             paddingY + gridHeight, lineColor);
+             paddingY + gridHeight, lineColour);
+  }
+}
+
+void Game::checkForSquareActivation() {
+  if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) &&
+      !ImGui::GetIO().WantCaptureMouse) {
+    Vector2 mousePos = GetMousePosition();
+    if (mousePos.x >= paddingX && mousePos.x < paddingX + gridWidth &&
+        mousePos.y >= paddingY && mousePos.y < paddingY + gridHeight) {
+      int col = (mousePos.x - paddingX) / squareSize;
+      int row = (mousePos.y - paddingY) / squareSize;
+      int index = getGridIndex(row, col);
+      activated[index] = !activated[index];
+      std::cout << "toggled square at " << row << " " << col << '\n';
+    }
   }
 }
--- src/include/game.hpp
+++ src/include/game.hpp
@@ -1,5 +1,8 @@
 #ifndef GAME_OF_LIFE
 #define GAME_OF_LIFE
+#include <iostream>
+#include <vector>
+
 #include "constants.hpp"
 #include "imgui.h"
 #include "raylib.h"
@@ -11,7 +14,18 @@ class Game {
   int rows{GameOfLifeConstants::defaultRowCount};
   int cols{GameOfLifeConstants::defaultColCount};
   Color backgroundColour{BLACK};
-  Color lineColor{WHITE};
+  Color lineColour{WHITE};
+  Color squareColour{WHITE};
+  float colourBackground[4]{0.0f, 0.0f, 0.0f, 1.0f};
+  float colourLines[4]{1.0f, 1.0f, 1.0f, 1.0f};
+  float colourSquares[4]{1.0f, 1.0f, 1.0f, 1.0f};
+  std::vector<bool> activated;
+
+  int squareSize;
+  int gridWidth;
+  int gridHeight;
+  int paddingX;
+  int paddingY;
 
  public:
   Game(const int screenWidth, const int screenHeight);
@@ -21,5 +35,10 @@ class Game {
  private:
   void options();
   void drawGrid();
+  void updateColors();
+  void draw();
+  void checkForSquareActivation();
+  void updateGridSize();
+  int getGridIndex(int row, int col);
 };
 #endif
